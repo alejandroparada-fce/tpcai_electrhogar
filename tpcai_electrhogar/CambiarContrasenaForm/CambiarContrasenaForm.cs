b2de7ef378d4c207233958e761b2ae74e80567b1
Facu098 <facugirardi@gmail.com>
2024-04-04 20:13:22 -0300

    Validacion menor en contraseña actual y nueva

--- tpcai_electrhogar/CambiarContrasenaForm/CambiarContrasenaForm.cs
+++ tpcai_electrhogar/CambiarContrasenaForm/CambiarContrasenaForm.cs
@@ -36,16 +36,22 @@ namespace tpcai_electrhogar
         {
             string mensajeError2 = "";
             string mensajeError = "";
+            string mensajeError4 = "";
             bool validar1 = Validaciones.ValidarContraseña(ContrasenaNueva.Text, "Nueva Contraseña", 8, 15, out string mensajeError1);
             bool validar2 = RepetirContrasena.Text == ContrasenaNueva.Text;
             bool validar3 = Validaciones.ValidarContraseña(ContrasenaActual.Text, "Contraseña Actual", 8, 15, out string mensajeError3);
+            bool validar4 = ContrasenaActual.Text == ContrasenaNueva.Text;
             if (!validar2)
             {
                 mensajeError2 = "Las contraseñas no coinciden.";
             }
-            
-            
-            mensajeError = mensajeError1 + "\n" + mensajeError2 + "\n" + mensajeError3;
+            else if (validar4)
+            {
+                mensajeError4 = "La nueva contraseña no puede ser la misma que la actual";
+            }
+
+
+            mensajeError = mensajeError1 + "\n" + mensajeError2 + "\n" + mensajeError3 + "\n" + mensajeError4;
             lblError.Text = mensajeError;
             //Falta hacer el llamado de logica de negocio para el guardado en base de datos, y el cambio de estado
         }
