e7cef930834fc9b68bb7510505a20140c6bd12ec
GabyFCE <duartegabriel2017@gmail.com>
2024-04-12 11:28:57 -0300

    Formulario de ventas

---
+++ tpcai_electrhogar.Datos/VentaEnt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpcai_electrhogar.Datos
+{
+    public class VentaEnt
+    {
+        private Guid _id;
+        private Guid _idCliente;
+        private Guid _idProducto;
+        private int _cantidad;
+        private DateTime _fechaAlta;
+        private int _estado;
+        private Guid _idUsuario;
+
+        public Guid Id {  get { return _id; } set { _id = value; } }
+        public Guid IdCliente { get { return _idCliente; } set { _idCliente = value; } }
+        public Guid IdProducto { get { return _idProducto; } set { _idProducto = value; } }
+        public int Cantidad { get { return _cantidad; } set { _cantidad = value; } }
+        public DateTime FechaAlta { get { return _fechaAlta; } set { _fechaAlta = value; } }
+        public int Estado { get { return _estado; } set { _estado = value; } }
+        public Guid IdUsuario { get {  return _idUsuario; } set { _idUsuario = value; } }
+
+        public VentaEnt(Guid IdCliente, Guid IdProducto, int Cantidad, DateTime FechaAlta, 
+            int Estado, Guid IdUsuario)
+        {
+            _id = Guid.NewGuid();
+            _idCliente = IdCliente;
+            _idProducto = IdProducto;
+            _cantidad = Cantidad;
+            _fechaAlta = FechaAlta;
+            _estado = Estado;
+            _idUsuario = IdUsuario;
+
+        }
+
+    }
+}
---
+++ tpcai_electrhogar/VentaForm/VentaForm.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace tpcai_electrhogar
+{
+    partial class VentaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VentaForm));
+            this.lblNombreEmpresa = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCalleNumero = new System.Windows.Forms.Label();
+            this.lblDirección = new System.Windows.Forms.Label();
+            this.lblDatosPersonales = new System.Windows.Forms.Label();
+            this.lblNombreApellido = new System.Windows.Forms.Label();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Código = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Descripción = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MontoUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MontoTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblNombrePromoción = new System.Windows.Forms.Label();
+            this.lblPromocion = new System.Windows.Forms.Label();
+            this.lblTotalPagar = new System.Windows.Forms.Label();
+            this.lblMontoPagar = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombreEmpresa
+            // 
+            this.lblNombreEmpresa.AutoSize = true;
+            this.lblNombreEmpresa.Font = new System.Drawing.Font("Verdana", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreEmpresa.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblNombreEmpresa.Location = new System.Drawing.Point(12, 32);
+            this.lblNombreEmpresa.Name = "lblNombreEmpresa";
+            this.lblNombreEmpresa.Size = new System.Drawing.Size(147, 23);
+            this.lblNombreEmpresa.TabIndex = 0;
+            this.lblNombreEmpresa.Text = "Electrohogar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(340, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fecha";
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDNI.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDNI.Location = new System.Drawing.Point(24, 24);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(47, 18);
+            this.lblDNI.TabIndex = 2;
+            this.lblDNI.Text = "DNI:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(99)))), ((int)(((byte)(196)))));
+            this.panel1.Controls.Add(this.lblNombreEmpresa);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 80);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(99)))), ((int)(((byte)(196)))));
+            this.panel2.Controls.Add(this.lblCalleNumero);
+            this.panel2.Controls.Add(this.lblDirección);
+            this.panel2.Controls.Add(this.lblDatosPersonales);
+            this.panel2.Controls.Add(this.lblNombreApellido);
+            this.panel2.Controls.Add(this.lblnumero);
+            this.panel2.Controls.Add(this.lblDNI);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 73);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblCalleNumero
+            // 
+            this.lblCalleNumero.AutoSize = true;
+            this.lblCalleNumero.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalleNumero.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblCalleNumero.Location = new System.Drawing.Point(328, 52);
+            this.lblCalleNumero.Name = "lblCalleNumero";
+            this.lblCalleNumero.Size = new System.Drawing.Size(131, 18);
+            this.lblCalleNumero.TabIndex = 7;
+            this.lblCalleNumero.Text = "Calle y número";
+            // 
+            // lblDirección
+            // 
+            this.lblDirección.AutoSize = true;
+            this.lblDirección.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDirección.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDirección.Location = new System.Drawing.Point(221, 52);
+            this.lblDirección.Name = "lblDirección";
+            this.lblDirección.Size = new System.Drawing.Size(82, 18);
+            this.lblDirección.TabIndex = 6;
+            this.lblDirección.Text = "Direción:";
+            // 
+            // lblDatosPersonales
+            // 
+            this.lblDatosPersonales.AutoSize = true;
+            this.lblDatosPersonales.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatosPersonales.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDatosPersonales.Location = new System.Drawing.Point(401, 3);
+            this.lblDatosPersonales.Name = "lblDatosPersonales";
+            this.lblDatosPersonales.Size = new System.Drawing.Size(158, 18);
+            this.lblDatosPersonales.TabIndex = 5;
+            this.lblDatosPersonales.Text = "Nombre y Apellido";
+            // 
+            // lblNombreApellido
+            // 
+            this.lblNombreApellido.AutoSize = true;
+            this.lblNombreApellido.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreApellido.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblNombreApellido.Location = new System.Drawing.Point(221, 3);
+            this.lblNombreApellido.Name = "lblNombreApellido";
+            this.lblNombreApellido.Size = new System.Drawing.Size(165, 18);
+            this.lblNombreApellido.TabIndex = 4;
+            this.lblNombreApellido.Text = "Nombre y Apellido:";
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblnumero.Location = new System.Drawing.Point(77, 24);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(71, 18);
+            this.lblnumero.TabIndex = 3;
+            this.lblnumero.Text = "Numero";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Código,
+            this.Descripción,
+            this.Cantidad,
+            this.MontoUnitario,
+            this.MontoTotal});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 153);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 129);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Código
+            // 
+            this.Código.HeaderText = "Código";
+            this.Código.Name = "Código";
+            this.Código.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Código.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Descripción
+            // 
+            this.Descripción.HeaderText = "Descripción";
+            this.Descripción.Name = "Descripción";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // MontoUnitario
+            // 
+            this.MontoUnitario.HeaderText = "Monto Unitario";
+            this.MontoUnitario.Name = "MontoUnitario";
+            // 
+            // MontoTotal
+            // 
+            this.MontoTotal.HeaderText = "Monto Total";
+            this.MontoTotal.Name = "MontoTotal";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblMonto);
+            this.panel3.Controls.Add(this.lblDescuento);
+            this.panel3.Controls.Add(this.lblNombrePromoción);
+            this.panel3.Controls.Add(this.lblPromocion);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 282);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 68);
+            this.panel3.TabIndex = 6;
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblMonto.Location = new System.Drawing.Point(649, 23);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(58, 18);
+            this.lblMonto.TabIndex = 6;
+            this.lblMonto.Text = "Monto";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescuento.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDescuento.Location = new System.Drawing.Point(541, 23);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(102, 18);
+            this.lblDescuento.TabIndex = 5;
+            this.lblDescuento.Text = "Descuento:";
+            // 
+            // lblNombrePromoción
+            // 
+            this.lblNombrePromoción.AutoSize = true;
+            this.lblNombrePromoción.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombrePromoción.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblNombrePromoción.Location = new System.Drawing.Point(118, 23);
+            this.lblNombrePromoción.Name = "lblNombrePromoción";
+            this.lblNombrePromoción.Size = new System.Drawing.Size(188, 18);
+            this.lblNombrePromoción.TabIndex = 4;
+            this.lblNombrePromoción.Text = "Nombre de Promoción";
+            // 
+            // lblPromocion
+            // 
+            this.lblPromocion.AutoSize = true;
+            this.lblPromocion.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPromocion.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblPromocion.Location = new System.Drawing.Point(12, 23);
+            this.lblPromocion.Name = "lblPromocion";
+            this.lblPromocion.Size = new System.Drawing.Size(100, 18);
+            this.lblPromocion.TabIndex = 3;
+            this.lblPromocion.Text = "Promoción:";
+            // 
+            // lblTotalPagar
+            // 
+            this.lblTotalPagar.AutoSize = true;
+            this.lblTotalPagar.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPagar.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblTotalPagar.Location = new System.Drawing.Point(517, 386);
+            this.lblTotalPagar.Name = "lblTotalPagar";
+            this.lblTotalPagar.Size = new System.Drawing.Size(126, 18);
+            this.lblTotalPagar.TabIndex = 7;
+            this.lblTotalPagar.Text = "Total a Pagar:";
+            // 
+            // lblMontoPagar
+            // 
+            this.lblMontoPagar.AutoSize = true;
+            this.lblMontoPagar.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontoPagar.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblMontoPagar.Location = new System.Drawing.Point(649, 386);
+            this.lblMontoPagar.Name = "lblMontoPagar";
+            this.lblMontoPagar.Size = new System.Drawing.Size(58, 18);
+            this.lblMontoPagar.TabIndex = 8;
+            this.lblMontoPagar.Text = "Monto";
+            // 
+            // VentaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(99)))), ((int)(((byte)(196)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMontoPagar);
+            this.Controls.Add(this.lblTotalPagar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VentaForm";
+            this.Opacity = 0.95D;
+            this.Text = "VentaForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombreEmpresa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDNI;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.Label lblNombreApellido;
+        private System.Windows.Forms.Label lblDatosPersonales;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCalleNumero;
+        private System.Windows.Forms.Label lblDirección;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblNombrePromoción;
+        private System.Windows.Forms.Label lblPromocion;
+        private System.Windows.Forms.Label lblTotalPagar;
+        private System.Windows.Forms.Label lblMontoPagar;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Código;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripción;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MontoUnitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MontoTotal;
+    }
+}
