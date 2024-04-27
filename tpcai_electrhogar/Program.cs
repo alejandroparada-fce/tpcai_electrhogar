499e471e228ba821661ba23217618438903786d5
alejandroparada-fce <164259943+alejandroparada-fce@users.noreply.github.com>
2024-04-27 20:41:30 -0300

    Fix botón "Cancelar" de FormProductoRegistrar.
    - Se vuelve al form de "Administrar Productos" en vez de abrir uno nuevo.

--- tpcai_electrhogar/Program.cs
+++ tpcai_electrhogar/Program.cs
@@ -19,13 +19,14 @@ namespace tpcai_electrhogar
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FormLogin());
             //Application.Run(new FormBienvenida());
-            //Application.Run(new FormMenuPrincipal());
+            Application.Run(new FormMenuPrincipal());
             //Application.Run(new FormUsuarioRegistrar());
-            Application.Run(new FormClienteListar());
+            //Application.Run(new FormClienteListar());
             //Application.Run(new FormClienteRegistrar());
             //Application.Run(new FormProveedorListar());
             //Application.Run(new FormProveedorRegistrar());
             //Application.Run(new FormAdmProductos());
+            //Application.Run(new FormProductoRegistrar());
             //Application.Run(new ListarBuscarUsuariosForm());
 
         }
